b37b199990d872fb0bc6dc2a13f15ae5ca0d3b2c
Bondo Pangaji <60285086+bondopangaji@users.noreply.github.com>
2021-10-19 14:44:28 +0700

    unit testing payroll entity

--- src/test/java/com/bondopangaji/emsapp/PayrollTests.java
+++ src/test/java/com/bondopangaji/emsapp/PayrollTests.java
@@ -24,13 +24,203 @@ SOFTWARE.
 
 **/
 
-
 package com.bondopangaji.emsapp;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.Rollback;
+
+import com.bondopangaji.emsapp.models.Employee;
+import com.bondopangaji.emsapp.models.Payroll;
+import com.bondopangaji.emsapp.repositories.PayrollRepository;
+import com.bondopangaji.emsapp.services.PayrollService;
 
 /**
  * @author bondopangaji
  *
  */
+
+@DisplayName("Test case for Payroll object")
+@ExtendWith(MockitoExtension.class)
 public class PayrollTests {
 
+	@Mock
+	private PayrollRepository payrollRepository;
+
+	@InjectMocks
+	private PayrollService payrollService;
+
+	Employee employee = new Employee();
+	
+	@BeforeEach
+	void SetUpEmployeeObject() {
+		employee.setEmployeeId(101);
+		employee.setRoleId(2);
+		employee.setFullName("Dummy Employee");
+		employee.setBirthDate("05-11-2001");
+		employee.setHomeAddress("Indonesia");
+		employee.setDomicileAddress("Indonesia");
+		employee.setPhoneNumber("62");
+		employee.setEmail("dummy.admin@bondopangji.com");
+		employee.setPassword("password");
+	}
+	
+	@Test
+	@Order(1)
+	@Rollback(value = false)
+	void NewPayroll() throws Exception {
+		Payroll expectedPayroll = new Payroll();
+		expectedPayroll.setPayrollId(1);
+		expectedPayroll.setEmployee(employee);
+		expectedPayroll.setPayrollDate("19-10-2021");
+		expectedPayroll.setHourlyRate(5);
+		expectedPayroll.setAnnualSalary(1800);
+		expectedPayroll.setTax(5);
+		expectedPayroll.setNetSalary(1710);
+
+		when(payrollRepository.save(expectedPayroll)).thenReturn(expectedPayroll);
+		payrollService.storeData(expectedPayroll);
+		payrollRepository.save(expectedPayroll);
+		
+		when(payrollRepository.getById((long) 1)).thenReturn(expectedPayroll);
+		Payroll actualPayroll = this.payrollRepository.getById((long) 1);
+		
+		Assertions.assertEquals(expectedPayroll, actualPayroll);
+	}
+	
+	@Test
+	@Order(2)
+	@Rollback(value = false)
+	void NewPayrollWithEmptyEmployee() throws Exception {
+		Throwable e = null;
+		String expectedMessage = null;
+		String actualMessage = "Employee cannot be null!";
+		
+		try {
+			Payroll expectedPayroll = new Payroll();
+			expectedPayroll.setPayrollId(1);
+			expectedPayroll.setEmployee(null);
+			expectedPayroll.setPayrollDate("19-10-2021");
+			expectedPayroll.setHourlyRate(5);
+			expectedPayroll.setAnnualSalary(1800);
+			expectedPayroll.setTax(5);
+			expectedPayroll.setNetSalary(1710);
+	
+			when(payrollRepository.save(expectedPayroll))
+				.thenThrow(new RuntimeException(actualMessage));
+			payrollService.storeData(expectedPayroll);
+			} catch (RuntimeException rex) {
+				e = rex;
+				expectedMessage = e.getMessage();
+			}
+			Assertions.assertTrue(e instanceof Exception);
+			Assertions.assertEquals(expectedMessage, actualMessage);
+	}
+	
+	@Test
+	@Order(3)
+	@Rollback(value = false)
+	void NewPayrollWithEmptyPayrollDate() throws Exception {
+		Throwable e = null;
+		String expectedMessage = null;
+		String actualMessage = "Payroll date cannot be null!";
+		
+		try {
+			Payroll expectedPayroll = new Payroll();
+			expectedPayroll.setPayrollId(1);
+			expectedPayroll.setEmployee(employee);
+			expectedPayroll.setPayrollDate("");
+			expectedPayroll.setHourlyRate(5);
+			expectedPayroll.setAnnualSalary(1800);
+			expectedPayroll.setTax(5);
+			expectedPayroll.setNetSalary(1710);
+	
+			when(payrollRepository.save(expectedPayroll))
+				.thenThrow(new RuntimeException(actualMessage));
+			payrollService.storeData(expectedPayroll);
+			} catch (RuntimeException rex) {
+				e = rex;
+				expectedMessage = e.getMessage();
+			}
+			Assertions.assertTrue(e instanceof Exception);
+			Assertions.assertEquals(expectedMessage, actualMessage);
+	}
+	
+	@Test
+	@Order(4)
+	@Rollback(value = false)
+	@SuppressWarnings("null")
+	void NewPayrollWithEmptyHourlyRate() throws NullPointerException {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Payroll expectedPayroll = new Payroll();
+			expectedPayroll.setPayrollId(1);
+			expectedPayroll.setEmployee(employee);
+			expectedPayroll.setPayrollDate("19-10-2021");
+			expectedPayroll.setHourlyRate((Long) null);
+			expectedPayroll.setAnnualSalary(1800);
+			expectedPayroll.setTax(5);
+			expectedPayroll.setNetSalary(1710);	    
+		});
+	}
+	
+	@Test
+	@Order(5)
+	@Rollback(value = false)
+	@SuppressWarnings("null")
+	void NewPayrollWithEmptyAnnualSalary() throws NullPointerException {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Payroll expectedPayroll = new Payroll();
+			expectedPayroll.setPayrollId(1);
+			expectedPayroll.setEmployee(employee);
+			expectedPayroll.setPayrollDate("19-10-2021");
+			expectedPayroll.setHourlyRate(5);
+			expectedPayroll.setAnnualSalary((Long) null);
+			expectedPayroll.setTax(5);
+			expectedPayroll.setNetSalary(1710);	    
+		});
+	}
+	
+	@Test
+	@Order(6)
+	@Rollback(value = false)
+	@SuppressWarnings("null")
+	void NewPayrollWithEmptyTax() throws NullPointerException {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Payroll expectedPayroll = new Payroll();
+			expectedPayroll.setPayrollId(1);
+			expectedPayroll.setEmployee(employee);
+			expectedPayroll.setPayrollDate("19-10-2021");
+			expectedPayroll.setHourlyRate(5);
+			expectedPayroll.setAnnualSalary(1800);
+			expectedPayroll.setTax((Integer) null);
+			expectedPayroll.setNetSalary(1710);	    
+		});
+	}
+	
+	@Test
+	@Order(7)
+	@Rollback(value = false)
+	@SuppressWarnings("null")
+	void NewPayrollWithEmptyNetSalary() throws NullPointerException {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Payroll expectedPayroll = new Payroll();
+			expectedPayroll.setPayrollId(1);
+			expectedPayroll.setEmployee(employee);
+			expectedPayroll.setPayrollDate("19-10-2021");
+			expectedPayroll.setHourlyRate(5);
+			expectedPayroll.setAnnualSalary(1800);
+			expectedPayroll.setTax(5);
+			expectedPayroll.setNetSalary((Integer) null);	    
+		});
+	}
+	
 }
